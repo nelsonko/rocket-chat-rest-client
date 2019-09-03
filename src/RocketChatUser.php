d7e12ebfe228899b9df8d97e8b60beb8c4895ecc
Manivannan Sadasivam <mani.s22@outlook.com>
2019-09-03 15:35:30 +0530

    [CUSTOM][ENH] Synergiq - RocketChat - Integration with the website

--- src/RocketChatUser.php
+++ src/RocketChatUser.php
@@ -1,21 +1,16 @@
 <?php
-
-namespace RocketChatsspp;
-
+namespace RocketChat;
 use Httpful\Request;
 use RocketChat\Client;
-
 class User extends Client {
-
 	public $username;
-	public $password;
+	private $password;
 	public $id;
 	public $nickname;
 	public $email;
 	public $customFields;
-  
+
 	public function __construct($username, $password, $fields = array()){
-		\Feedback::success('Reached RocketChat condition');
 		parent::__construct();
 		$this->username = $username;
 		$this->password = $password;
@@ -31,14 +26,28 @@ class User extends Client {
 	}
 
 	/**
-	* Authenticate with the REST API.
-	*/
+	 * Gets a user’s information, limited to the caller’s permissions.
+	 */
+	public function info() {
+		$response = Request::get( $this->api . 'users.info?userId=' . $this->id )->send();
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			$this->id = $response->body->user->_id;
+			$this->nickname = $response->body->user->name;
+			$this->email = $response->body->user->emails[0]->address;
+			return $response->body;
+		} else {
+			throw $this->createExceptionFromResponse($response, "Could not get user's information");
+		}
+	}
+
+	/**
+	 * Authenticate with the REST API.
+	 */
 	public function login($save_auth = true) {
-		\Feedback::success('Reached API login method');
-		/*$response = Request::post( $this->api . 'login' )
+		// $response = Request::post( $this->api . 'login' )
+		$response = Request::post( 'http://localhost:3000/api/v1/login' )
 			->body(array( 'user' => $this->username, 'password' => $this->password ))
 			->send();
-
 		if( $response->code == 200 && isset($response->body->status) && $response->body->status == 'success' ) {
 			if( $save_auth) {
 				// save auth token for future requests
@@ -51,42 +60,32 @@ class User extends Client {
 			return $response->body->data;
 		} else {
 			throw $this->createExceptionFromResponse($response, "Could not authenticate with the REST API");
-		}*/
-	}
-
-	/**
-	* Gets a user’s information, limited to the caller’s permissions.
-	*/
-	public function info() {
-		$response = Request::get( $this->api . 'users.info?userId=' . $this->id )->send();
-
-		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
-			$this->id = $response->body->user->_id;
-			$this->nickname = $response->body->user->name;
-			$this->email = $response->body->user->emails[0]->address;
-			return $response->body;
-		} else {
-			throw $this->createExceptionFromResponse($response, "Could not get user's information");
 		}
 	}
 
+	public function userid($uname) {
+		$response = Request::get( 'http://localhost:3000/api/v1/users.info?username=' . $uname )->send();
+		return $response->body->user->_id;
+	}
+
 	/**
-	* Create a new user.
-	* for customFields we have to send json-object so we use json_decode with inner json_encode
-	*/
+	 * Create a new user.
+	 * for customFields we have to send json-object so we use json_decode with inner json_encode
+	 */
 	public function create() {
 		$bodydata=array(
-				'name' => $this->nickname,
-				'email' => $this->email,
-				'username' => $this->username,
-				'password' => $this->password,
+			'name' => $this->nickname,
+			'email' => $this->email,
+			'username' => $this->username,
+			'password' => $this->password,
 		);
 		if (isset($this->customFields))
-		   $bodydata['customFields'] = json_decode(json_encode($this->customFields), JSON_FORCE_OBJECT);
-		$response = Request::post( $this->api . 'users.create' )
+			$bodydata['customFields'] = json_decode(json_encode($this->customFields), JSON_FORCE_OBJECT);
+//		$response = Request::post( $this->api . 'users.create' )
+		$response = Request::post( 'http://localhost:3000/api/v1/users.create' )
 			->body($bodydata)
 			->send();
-
+//		echo json_encode($response);
 		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
 			$this->id = $response->body->user->_id;
 			return $response->body->user;
@@ -97,43 +96,123 @@ class User extends Client {
 	}
 
 	/**
-	* Update a user.
-	* for data we have to send json-object so we use json_decode with inner json_encode
-	* using the attributes of this class as update-values
-	*/
-	public function update() {
-		$response = Request::post( $this->api . 'users.update' )
+	 * Update a user.
+	 * for data we have to send json-object so we use json_decode with inner json_encode
+	 * using the attributes of this class as update-values
+	 */
+	public function update($rc_display_name, $rc_username, 	$id) {
+
+		// Update User
+		$response = Request::post( 'http://localhost:3000/api/v1/users.update' )
 			->body(array(
-				'userId' => $this->id,
-				'data' => json_decode(json_encode(array('name' => $this->nickname,'email'=>$this->email,'username'=>$this->username,'password'=>$this->password), JSON_FORCE_OBJECT)),
+				'userId' => $id,
+				'data' => json_decode(json_encode(array('name' => $rc_display_name, 'username' => $rc_username), JSON_FORCE_OBJECT)),
 			))
 			->send();
+		return $response->body->success;
+		/*$response = Request::post( $this->api . 'users.update' )
+					->body(array(
+						'userId' => $this->id,
+						'data' => json_decode(json_encode(array('name' => $this->nickname,'email'=>$this->email,'username'=>$this->username,'password'=>$this->password), JSON_FORCE_OBJECT)),
+					))
+					->send();*/
+		/*if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			$this->id = $response->body->user->_id;
+			return $response->body->user;
+		} else {
+			throw $this->createExceptionFromResponse($response, "Could not create new user");
+		}*/
+	}
 
+	/**
+	 * Deletes an existing user.
+	 */
+	public function delete($args) {
+		// get user ID if needed
+		/*if( !isset($this->id) ){
+			$this->me();
+		}*/
+//		$response = Request::post( $this->api . 'users.delete' )
+		/*$response = Request::post( 'http://localhost:3000/api/v1/users.delete' )
+			->body(array('userId' => $this->id))
+			->send();*/
+		$response = Request::post( 'http://localhost:3000/api/v1/users.delete' )
+			->body(array('username' => $args['realName']))
+			->send();
+		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
+			return true;
+		} else {
+			throw $this->createExceptionFromResponse($response, "Could not delete user");
+		}
+	}
+
+
+	public static function getStatus($user) {
+		// Get User Presence
+		$response = Request::get( 'http://localhost:3000/api/v1/users.getPresence?username='.$user )->send();
+		$presence = $response->body->presence;
+
+		// Get User Info u5KDkZsLEpHr8JrRc
+		// $response = Request::get( 'http://localhost:3000/api/v1/users.info?userId='.$user )->send();
+		// $presence = $response->body->user->statusText;
+
+		return $presence;
+	}
+
+	public static function setStatus($rc_user, $set) {
+		$response = Request::post( 'http://localhost:3000/api/v1/users.setStatus' )
+			->body(array(
+				'userId' => $rc_user,
+				'status' => $set,
+				'message' => $set,
+			))
+			->send();
+		return $response;
+	}
+
+	public function create_group($args) {
+		$bodydata=array(
+			'name' => $args
+		);
+		if (isset($this->customFields))
+			$bodydata['customFields'] = json_decode(json_encode($this->customFields), JSON_FORCE_OBJECT);
+//		$response = Request::post( $this->api . 'users.create' )
+		$response = Request::post( 'http://localhost:3000/api/v1/groups.create' )
+			->body($bodydata)
+			->send();
+//		echo json_encode($response);
 		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
 			$this->id = $response->body->user->_id;
 			return $response->body->user;
 		} else {
-            throw $this->createExceptionFromResponse($response, "Could not create new user");
+			echo( $response->body->error . "\n" );
+			return false;
 		}
 	}
 
-	/**
-	* Deletes an existing user.
-	*/
-	public function delete() {
-
-		// get user ID if needed
-		if( !isset($this->id) ){
-			$this->me();
-		}
-		$response = Request::post( $this->api . 'users.delete' )
-			->body(array('userId' => $this->id))
+	/*public function create_group($args) {
+		$response = Request::post( 'http://localhost:3000/api/v1/groups.create' )
+			->body($bodydata)
 			->send();
+	}*/
 
-		if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
-			return true;
-		} else {
-            throw $this->createExceptionFromResponse($response, "Could not delete user");
+
+	public static function users() {
+		// Get User Presence
+		$rc_users = array();
+		$users = Request::get( 'http://localhost:3000/api/v1/users.list' )->send();
+		foreach($users->body->users AS $user) {
+			if ($user->username != 'admin' && $user->username != 'rocket.cat') {
+				/*echo $user->username;
+				echo $user->name;
+				print_r($user->emails[0]->address);
+				echo "\n";*/
+				$rc_users[] = $user->name;
+			}
 		}
+		return $rc_users;
 	}
+
+
+
 }
